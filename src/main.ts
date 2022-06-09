5452f43354ec2609e04ac4ee643a7b3f3c44d08e
Santiago Barquero <sbarquero@gmail.com>
2022-06-09 19:44:50 +0200

    Change port and adds Bootstrap, Bootstrap Icon and SASS packages

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
+// Imports Bootstrap
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap";
+
 import App from "./App.vue";
 import router from "./router";
 
