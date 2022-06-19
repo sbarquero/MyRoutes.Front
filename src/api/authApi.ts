93c59d6be8f978dd9de599b0078797199b4a27f4
Santiago Barquero <sbarquero@gmail.com>
2022-06-19 09:38:19 +0200

    Extract baseURL to .env configuration file and print Vite environment variables in about

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const authApi = axios.create({
-  baseURL: 'http://localhost:3000/auth',
+  baseURL: import.meta.env.VITE_AUTH_BASE_URL,
 });
 
 export default authApi;
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,29 @@
 <template>
   <div>
     <h1>This is the About page</h1>
+    <h4>Vite environment variables</h4>
+    <ul>
+      <li>
+        import.meta.env.MODE: <b>{{ MODE }}</b>
+      </li>
+      <li>
+        import.meta.env.BASE_URL: <b>{{ BASE_URL }}</b>
+      </li>
+      <li>
+        import.meta.env.DEV: <b>{{ DEV }}</b>
+      </li>
+      <li>
+        import.meta.env.PROD: <b>{{ PROD }}</b> (opposite of import.meta.env.DEV)
+      </li>
+    </ul>
   </div>
 </template>
+
+<script setup lang="ts">
+const MODE = import.meta.env.MODE;
+const BASE_URL = import.meta.env.BASE_URL;
+const DEV = import.meta.env.DEV;
+const PROD = import.meta.env.PROD;
+</script>
 
 <style></style>
