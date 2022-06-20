c40c20173397b41614428a3c57a47056ff278d54
Santiago Barquero <sbarquero@gmail.com>
2022-06-20 21:29:25 +0200

    Refresh token before check authorization in  beforeEach route

--- src/router/index.ts
+++ src/router/index.ts
@@ -52,6 +52,16 @@ const router = createRouter({
 
 router.beforeEach(async to => {
   const auth = useAuthStore();
+
+  // Refresh token if exist refreshToken
+  if (localStorage.getItem('refreshToken')) {
+    const result = await auth.refresh();
+    if (!result?.ok) {
+      console.log(result?.message);
+    }
+  }
+
+  // Check authorization
   if (to.meta.requiresAuth) {
     if (auth.rol != 'admin' && to.meta.authorizedRol !== auth.rol) {
       return { name: 'not-authorized' };
