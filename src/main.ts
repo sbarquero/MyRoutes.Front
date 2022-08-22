799e07dde51379fbd72c41b877da63a14d999640
Santiago Barquero <sbarquero@gmail.com>
2022-08-22 20:24:04 +0200

    Remove MapxBoxGL, OpenLayer and Vue3OpenLayer

--- src/main.ts
+++ src/main.ts
@@ -9,14 +9,6 @@ import 'bootstrap';
 import 'sweetalert2/dist/sweetalert2.all.min.js';
 // import "sweetalert2/dist/sweetalert2.min.css";
 
-// Imports Vue3-Openlayers
-// import OpenLayersMap from 'vue3-openlayers';
-// import 'vue3-openlayers/dist/vue3-openlayers.css';
-
-// MapBox
-import mapboxgl from 'mapbox-gl';
-mapboxgl.accessToken = import.meta.env.VITE_MAPBOXGL_ACCESS_TOKEN;
-
 import App from './App.vue';
 import i18n from './i18n/i18n';
 import router from './router';
@@ -26,6 +18,5 @@ const app = createApp(App);
 app.use(createPinia());
 app.use(router);
 app.use(i18n);
-// app.use(OpenLayersMap);
 
 app.mount('#app');
