209d0e7a879c2e5094fb7c8e0f30a098dba87f8f
Santiago Barquero <sbarquero@gmail.com>
2022-08-28 16:51:40 +0200

    Add zoom information and change zoom in/decrements

--- src/components/home/MapView.vue
+++ src/components/home/MapView.vue
@@ -8,6 +8,8 @@
   <div v-else class="map-container">
     <!-- Map Div -->
     <div id="mapContainer" class="map" ref="mapElement"></div>
+    <!-- Zoom tag-->
+    <div class="zoom"><b>Zoom: </b>{{ zoom }}</div>
     <!-- Location tag-->
     <div class="fixed-bottom d-flex justify-content-center">
       <div class="location">
@@ -34,6 +36,9 @@ const mapStore = useMapStore();
 const mapElement = ref<HTMLDivElement>();
 const trackStore = useTrackStore();
 const { hideTrackIndex, selectedTrackIndex } = storeToRefs(useTrackStore());
+
+const initialZoom = 16;
+const zoom = ref(initialZoom);
 
 let map: L.Map;
 
@@ -80,7 +85,7 @@ async function initMap() {
     {
       attribution:
         'Map data (c) <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-      minZoom: 3,
+      minZoom: 2,
       maxZoom: 20,
       id: 'mapbox/streets-v11',
       accessToken: import.meta.env.VITE_MAPBOX_ACCESS_TOKEN,
@@ -90,14 +95,14 @@ async function initMap() {
   // const streets = L.tileLayer(mapboxUrl, {id: 'mapbox/streets-v11', tileSize: 512, zoomOffset: -1, attribution: mapboxAttribution});
 
   var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    minZoom: 3,
+    minZoom: 2,
     maxZoom: 19,
     attribution:
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   });
 
   const openTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-    minZoom: 3,
+    minZoom: 2,
     maxZoom: 17,
     attribution:
       'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
@@ -110,11 +115,18 @@ async function initMap() {
     'Open topo map': openTopoMap,
   };
 
-  map = L.map(mapElement.value).setView(userLocation.value, 16);
+  map = L.map(mapElement.value, {
+    zoomSnap: 0.25, // Forces the map's zoom level to always be a multiple of this (default = 1)
+    zoomDelta: 0.5, // Zoom level using keyboard or zoom controls
+    wheelPxPerZoomLevel: 128, // Zoom level in pixels using mouse wheel
+  }).setView(userLocation.value, initialZoom);
   osm.addTo(map);
 
+  map.on('zoom', () => {
+    zoom.value = map.getZoom();
+  });
   L.control.layers(baseMaps).addTo(map);
-  L.control.scale({ position: 'bottomleft' }).addTo(map);
+  L.control.scale().addTo(map);
 
   L.marker(userLocation.value).addTo(map).bindPopup('<h5>My Location</h5>').openPopup();
 }
@@ -146,10 +158,27 @@ async function initMap() {
   z-index: 1;
 }
 
-.location {
-  background-color: rgba(255, 255, 255, 0.9);
+.zoom {
+  background-color: rgba(255, 255, 255, 0.7);
   border-radius: 0.5rem;
-  bottom: 1rem;
+  /* offset-x | offset-y | blur-radius | spread-radius | color */
+  box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.2);
+  color: #444;
+  font-size: 1rem;
+  left: 3.5rem;
+  margin: auto;
+  padding: 0 0.5rem;
+  position: absolute;
+  text-align: center;
+  top: 3.75rem;
+  width: auto;
+  z-index: 2;
+}
+
+.location {
+  background-color: rgba(255, 255, 255, 0.7);
+  border-radius: 0.5rem;
+  bottom: 1.5rem;
   /* offset-x | offset-y | blur-radius | spread-radius | color */
   box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.2);
   color: #444;
