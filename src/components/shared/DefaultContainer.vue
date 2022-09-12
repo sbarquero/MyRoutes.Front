49120962e856b6149f94496dd6fb4e34e7de7f91
Santiago Barquero <sbarquero@gmail.com>
2022-09-12 17:28:31 +0200

    Create the DefaultContainer component and use it with the UserView

---
+++ src/components/shared/DefaultContainer.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="map-background">
+    <div class="container-fluid container-lg mt-0">
+      <h1 class="title"><slot name="title"></slot></h1>
+      <div class="card p-4 mt-4">
+        <slot name="content"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.map-background {
+  background: url('@/assets/img/world-topo-map.png') no-repeat center;
+  background-size: cover;
+  height: calc(100% - 3rem);
+}
+
+.card {
+  background-color: rgba(255, 255, 255, 0.7);
+  border-radius: 10px;
+  border-radius: 0.35rem;
+}
+
+.title {
+  background-color: rgba(255, 255, 255, 0.7);
+  border-radius: 10px;
+  border-radius: 0.35rem;
+  display: inline-block;
+  margin-top: 1rem;
+  padding: 0.1rem 1rem 0.25rem 1rem;
+}
+</style>
--- src/views/UsersView.vue
+++ src/views/UsersView.vue
@@ -1,32 +1,31 @@
 <template>
-  <div class="map-background">
-    <div class="container-fluid container-lg mt-0">
-      <h1>{{ t('userView.title') }}</h1>
-      <div class="card p-4 mt-4">
-        <div class="row">
-          <div class="col-12 col-md-5 col-lg-4">
-            <UserList />
-          </div>
-          <div v-if="userStore.userEditing" class="dark"></div>
-          <div class="col-12 col-md-7 col-lg-8 mt-5 mt-md-0">
-            <UserCard />
-          </div>
+  <DefaultContainer>
+    <template #title>{{ t('userView.title') }}</template>
+    <template #content>
+      <div class="row">
+        <div class="col-12 col-md-5 col-lg-4">
+          <UserList />
         </div>
-        <AddButton
-          v-if="!userStore.userEditing"
-          :tooltip="t('userView.addButton.tooltip')"
-          @click="onAddUser"
-          class="add-button"
-        />
+        <div v-if="userStore.userEditing" class="dark"></div>
+        <div class="col-12 col-md-7 col-lg-8 mt-5 mt-md-0">
+          <UserCard />
+        </div>
       </div>
-    </div>
-  </div>
+      <AddButton
+        v-if="!userStore.userEditing"
+        :tooltip="t('userView.addButton.tooltip')"
+        @click="onAddUser"
+        class="add-button"
+      />
+    </template>
+  </DefaultContainer>
 </template>
 
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n';
 import { useUserStore } from '@/stores/userStore';
 import AddButton from '@/components/shared/AddButton.vue';
+import DefaultContainer from '../components/shared/DefaultContainer.vue';
 import UserCard from '../components/users/UserCard.vue';
 import UserList from '../components/users/UserList.vue';
 
@@ -39,26 +38,6 @@ const onAddUser = async () => {
 </script>
 
 <style scoped>
-.map-background {
-  background: url('@/assets/img/world-topo-map.png') no-repeat center;
-  background-size: cover;
-  height: calc(100% - 3rem);
-}
-
-.card {
-  background-color: rgba(255, 255, 255, 0.7);
-  border-radius: 10px;
-  border-radius: 0.35rem;
-}
-
-h1 {
-  background-color: rgba(255, 255, 255, 0.7);
-  border-radius: 10px;
-  border-radius: 0.35rem;
-  display: inline-block;
-  margin-top: 1rem;
-  padding: 0.1rem 1rem 0.25rem 1rem;
-}
 .dark {
   background-color: rgba(0, 0, 0, 0.2);
   pointer-events: none;
