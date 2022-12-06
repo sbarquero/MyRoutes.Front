4505d216845197d891948e6af7f7a0c61edaabf1
Santiago Barquero <sbarquero@gmail.com>
2022-12-06 11:46:46 +0100

    Suppresses warning of Vue-i18n

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,11 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  define: {
+    // Suppresses warning of Vue-i18n
+    // See: https://www.appsloveworld.com/vuejs/100/2/you-are-running-the-esm-bundler-build-of-vue-i18n-it-is-recommended-to-configure
+    __VUE_I18N_FULL_INSTALL__: true,
+    __VUE_I18N_LEGACY_API__: false,
+    __INTLIFY_PROD_DEVTOOLS__: false,
+  },
 });
